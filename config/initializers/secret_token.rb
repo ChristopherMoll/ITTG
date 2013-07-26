03a1e009665bb67406de9424f2767a0a7eaa6ea2
vagrant <vagrant@precise32.(none)>
2013-07-26 00:18:03 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ITTG::Application.config.secret_key_base = 'c9353c499df2c6c2513b506545d11d0411c498f1e66e7cfb9e8413fbbf45aee09d8b053b16e316c9b2eec8f6ef85339a06c5af7cd2cacd8b6b7d4eff93c8ff6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ITTG::Application.config.session_store :cookie_store, key: '_ITTG_session'
